ea8863d1a1dce8658495ef923c46fc7f141d65d7
Cmiranda-g <car.mirandag@duocuc.cl>
2025-05-13 10:57:16 -0400

    edutech

---
+++ msvc.profesor/src/main/java/com/edutech/msvc/profesor/Application.java
@@ -0,0 +1,13 @@
+package com.edutech.msvc.profesor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
